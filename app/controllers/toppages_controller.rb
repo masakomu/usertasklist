ad5c85612e328217280600d46a4106734f9b5b20
masako.komukai <masako.komukai@gmail.com>
2018-07-04 12:22:40 +0000

    login

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,6 @@
 class ToppagesController < ApplicationController
   def index
+      @user = current_user
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180704082652) do
+ActiveRecord::Schema.define(version: 20180704113035) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.string   "status"
-    t.integer  "user_id"
+    t.string   "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
